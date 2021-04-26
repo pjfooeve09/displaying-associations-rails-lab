8c718ec3ecefc5f05a7ce836ef5e01aba2aba852
Parham Javadi <pjfooeve09@yahoo.com>
2021-04-26 16:33:26 -0700

    commit tree f3db04c46c02995b8a8a0a870fc7206a11552382

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,12 @@
+
 class Song < ActiveRecord::Base
   belongs_to :artist
+
+  def artist_name
+    self.artist.name
+  end
+
+  def artist_title
+    self.artist.name.titleize + " - " + self.title.titleize
+  end
 end
